bb8bfab4319348da4e03054939f73d4e7973961a
Mingjaam <mingjaam@gmail.com>
2026-02-28 10:00:00 +0900

    feat: 앱 진입점 추가

---
+++ heartsignal/heartsignalApp.swift
@@ -0,0 +1,17 @@
+//
+//  heartsignalApp.swift
+//  heartsignal
+//
+//  Created by 김민재 on 4/21/26.
+//
+
+import SwiftUI
+
+@main
+struct heartsignalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
