cfa68d80792d4fbc19e8c65230fb7fc51dcd4eb5
Mingjaam <mingjaam@gmail.com>
2026-04-07 10:00:00 +0900

    feat: 앱 진입점에 연동 매니저 주입

--- heartsignal/heartsignalApp.swift
+++ heartsignal/heartsignalApp.swift
@@ -9,9 +9,12 @@ import SwiftUI
 
 @main
 struct heartsignalApp: App {
+    @StateObject private var connectivity = PhoneConnectivityManager.shared
+
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environmentObject(connectivity)
         }
     }
 }
