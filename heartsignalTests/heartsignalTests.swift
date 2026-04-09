d16ba4f880fa01513dff3606c5fd3c2c682c5a37
Mingjaam <mingjaam@gmail.com>
2026-04-09 10:00:00 +0900

    test: Unit Test 및 UI Test 추가

---
+++ heartsignalTests/heartsignalTests.swift
@@ -0,0 +1,19 @@
+//
+//  heartsignalTests.swift
+//  heartsignalTests
+//
+//  Created by 김민재 on 4/21/26.
+//
+
+import Testing
+@testable import heartsignal
+
+struct heartsignalTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
---
+++ heartsignalWatch Watch AppTests/heartsignalWatch_Watch_AppTests.swift
@@ -0,0 +1,19 @@
+//
+//  heartsignalWatch_Watch_AppTests.swift
+//  heartsignalWatch Watch AppTests
+//
+//  Created by 김민재 on 4/26/26.
+//
+
+import Testing
+@testable import heartsignalWatch_Watch_App
+
+struct heartsignalWatch_Watch_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
